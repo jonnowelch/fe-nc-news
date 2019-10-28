5237021b8a45b2ed58beff05fb4de13a48d90c88
jonnowelch <jonnowelch@yahoo.com>
2019-10-28 10:56:24 +0000

    app created

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Components/Header';
+import './App.css';
+import Main from './Components/Main';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
