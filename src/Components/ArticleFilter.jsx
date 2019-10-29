d36bbc4c23424ad5de0050ff2931041b6c69409d
jonnowelch <jonnowelch@yahoo.com>
2019-10-29 09:43:36 +0000

    changing article list from a table to indiv article cards

---
+++ src/Components/ArticleFilter.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import * as API from '../API';
+
+export default class ArticleFilter extends Component {
+  state = {
+    articles: [],
+    date_created: '',
+    comment_count: 0,
+    votes: 0
+  };
+
+  //   componentDidMount() {
+
+  //   }
+
+  render() {
+    return <div></div>;
+  }
+}
--- src/Components/SingleArticle.jsx
+++ src/Components/SingleArticle.jsx
@@ -5,42 +5,45 @@ import ArticleVote from './ArticleVote';
 
 export default class SingleArticle extends Component {
   state = {
-    indivArticle: {}
+    indivArticle: {},
+    comment_count: 0,
+    author: '',
+    created_at: ''
   };
 
   componentDidMount() {
     API.getIndividualArticle(this.props.article_id).then(response => {
       this.setState({
-        indivArticle: response.article
+        indivArticle: response.article,
+        comment_count: response.article.comment_count,
+        author: response.article.author,
+        created_at: response.article.created_at
       });
     });
   }
 
+  // where does this.props come from here?
   render() {
-    // console.log(this.props);
+    console.log(this.state);
     return (
       <div className="grid-container" id="singleArticle">
         <div className="grid-item item1"> {this.state.indivArticle.body}</div>
         <div className="grid-item item2">
           <ArticleVote article_id={this.props.article_id} />
         </div>
-        <div className="grid-item item3">Author: </div>
-        <div className="grid-item item4">Created At</div>
+        <div className="grid-item item3">
+          Author: {this.state.indivArticle.author}
+        </div>
+        <div className="grid-item item4">
+          Created At: {this.state.indivArticle.created_at}
+        </div>
         <div className="grid-item item5">
           <CommentViewer article_id={this.props.article_id} />
+        </div>
+        <div className="grid-item item6">
+          Comment Count: {this.state.comment_count}
         </div>
       </div>
     );
   }
 }
-
-/* // return (
-//     <div class="grid-container" id="singleArticle">
-//       {this.state.indivArticle.body}
-//       <br></br>
-//       {this.state.indivArticle.votes}
-//       <CommentViewer article_id={this.props.article_id} />
-//       <ArticleVote article_id={this.props.article_id} />
-//     </div>
-//   );
-// } */
