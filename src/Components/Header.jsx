68c922419a27b2830f2bfefee15ac3232cb5f90e
jonnowelch <49447225+jonnowelch@users.noreply.github.com>
2020-01-20 18:17:47 +0000

    updated header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import UsersList from "./UsersList";
 import LogoutUser from "./LogoutUser";
 import { Link } from "@reach/router";
-import LoginUser from "./LoginUser";
 
 export default function Header({
   loggedInUser,
@@ -12,16 +11,12 @@ export default function Header({
   return (
     <h1 className="header-container">
       <Link to={"/"}>NC News </Link>
-      <div className="login-container">
-        <div className="login-text">Logged in : {loggedInUser} </div>
+      <div className="log-in-container">
+        <div className="log-in-text">Logged in : {loggedInUser} </div>
         <UsersList
-          className="users-list"
+          className="log-in-button"
           loginUserFunction={loginUserFunction}
           loggedInUser={loggedInUser}
-        />
-        <LoginUser
-          className="login-button"
-          loginUserFunction={loginUserFunction}
         />
         <LogoutUser
           className="logout-button"
