6009fa6a75a838a41ae58873489c2f0b2ab0af66
jonnowelch <49447225+jonnowelch@users.noreply.github.com>
2020-01-20 18:19:19 +0000

    updated loading

--- src/Components/Loading.jsx
+++ src/Components/Loading.jsx
@@ -1,20 +1,13 @@
 import React from "react";
-<<<<<<< HEAD
 import logo from "../loading.jpg";
-=======
-import logo from "../loading.gif";
->>>>>>> 0c9a4227b8673599871fa3c613b8d0f39419dfe0
 
 const Loading = () => {
   return (
     <div>
-<<<<<<< HEAD
       <img src={logo} alt="loading" />
-=======
-      Loading... <img src={logo} alt="loading..." />
->>>>>>> 0c9a4227b8673599871fa3c613b8d0f39419dfe0
     </div>
   );
 };
 
 export default Loading;
+
